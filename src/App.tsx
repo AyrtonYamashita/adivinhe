a75c83867f21d540949774498868a6770210069e
Ayrton Yamashita <ayrtonyamashita@gmail.com>
2025-05-22 16:52:43 -0400

    Listando letras utilizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
+import styles from "./app.module.css";
+
+import { useEffect, useState } from "react";
+import { WORDS, Challenge } from "./utils/words";
 import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
-import styles from "./app.module.css";
 import { Letter } from "./components/Letter";
 import { Input } from "./components/Input";
 import { Button } from "./components/Button";
-import { LettersUsed } from "./components/LettersUsed";
+import { LettersUsed, LetterUsedProps } from "./components/LettersUsed";
 
 function App() {
+  const [attempts, setAttempts] = useState(0);
+  const [letter, setLetter] = useState("");
+  const [challenge, setChallenge] = useState<Challenge | null>(null);
+  const [letterUsed, setletterUsed] = useState<LetterUsedProps[]>([]);
+
   function handleRestartGame() {
     alert("Reiniciar o jogo!");
   }
+
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length);
+    const randomWord = WORDS[index];
+    setChallenge(randomWord);
+    setAttempts(0);
+    setLetter("");
+  }
+
+  useEffect(() => {
+    startGame();
+  }, []);
+
+  if (!challenge) return;
+
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame} />
-        <Tip tip="Uma das linguagens de programação mais utilizadas" />
+        <Header current={attempts} max={10} onRestart={handleRestartGame} />
+        <Tip tip={challenge.tip} />
         <div className={styles.words}>
-          <Letter value="R" />
-          <Letter value="E" />
-          <Letter value="A" />
-          <Letter value="C" />
-          <Letter value="T" />
+          {challenge.word.split("").map(() => (
+            <Letter />
+          ))}
         </div>
 
         <h4>Palpite</h4>
@@ -29,7 +50,7 @@ function App() {
           <Button title="Confirmar" />
         </div>
 
-        <LettersUsed />
+        <LettersUsed data={letterUsed} />
       </main>
     </div>
   );
